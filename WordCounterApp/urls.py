246a25ad956d570c3777d0061043611d8380e3fe
ahad-emu <ahademu99@gmail.com>
2020-06-28 17:27:27 +0600

    change views.py file, add about page, add navbar

--- WordCounterApp/urls.py
+++ WordCounterApp/urls.py
@@ -4,4 +4,5 @@ from WordCounterApp import views
 urlpatterns = [
     path("", views.homepage, name="index"),
     path("counter/", views.wordcounter, name="counter"),
+    path("about/", views.about, name="about"),
 ]
--- WordCounterApp/views.py
+++ WordCounterApp/views.py
@@ -5,14 +5,27 @@ import re
 def homepage(request):
     return render(request, "index.html",)
 
+def about(request):
+    return render(request, "about.html")
+
 def wordcounter(request):
     fulltext = request.POST["fulltext"]
-    textlist = re.split(', |_|-|!| |',fulltext)
+    fulltext = fulltext.strip()
+    textlist = re.split(', |_|-|!|@|#|$|%|"|&| |',fulltext)
     wordlength = len(textlist)
+    new_text_list = []
+    i = 0
+    while wordlength > i:
+        if textlist[i] == "":
+            i = i + 1
+            continue
+        new_text_list.append(textlist[i])
+        i = i + 1
+    wordlength = len(new_text_list)
     wordDictionaries = {}
-    for word in textlist:
-        if word in wordDictionaries:
-            wordDictionaries[word] += 1
+    for word in new_text_list:
+        if word.lower() in wordDictionaries:
+            wordDictionaries[word.lower()] += 1
         else:
-            wordDictionaries[word] = 1
+            wordDictionaries[word.lower()] = 1
     return render(request, "counter.html", {'wordlength': wordlength, 'fulltext': fulltext,'wordDictionaries': wordDictionaries})
